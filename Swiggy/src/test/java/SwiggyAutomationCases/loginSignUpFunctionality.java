6bf5659a259262a91dd58ae5c3137a370d03ba25
SYSTEMS\awais.sultan <042waisi@gmail.com>
2021-06-30 14:19:31 +0500

    SignUp

---
+++ Swiggy/src/test/java/SwiggyAutomationCases/loginSignUpFunctionality.java
@@ -0,0 +1,70 @@
+package SwiggyAutomationCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class loginSignUpFunctionality {
+	
+	loginSignUpFunctionality loginSignUp_;
+	public static WebDriver driver;
+	
+	@FindBy(how = How.XPATH, xpath = "/html/body/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div[1]/div/a[2]")
+	public WebElement signUp_;
+
+	@FindBy(how = How.ID, using = "mobile")
+	public WebElement PhoneNumber;
+
+	@FindBy(how = How.ID, using = "email")
+	public WebElement Email;
+
+	@FindBy(how = How.ID, using = "name")
+	public WebElement Name;
+
+	@FindBy(how = How.ID, using = "password")
+	public WebElement Password;
+
+	@FindBy(how = How.XPATH, xpath = "/html/body/div[2]/div/div/div[2]/div/div/div/div[2]/form/div[2]/a")
+	public WebElement continueBtn;
+	
+	public void launchBrowser() throws Throwable {
+		try {
+			System.setProperty("webdriver.chrome.driver",
+					"C:/Users/awais.sultan/git/TPG/TpgAutomation/chromeDriver/chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			loginSignUp_ = PageFactory.initElements(driver, loginSignUpFunctionality.class);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void launchSwiggy() throws Throwable {
+
+		try {
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.get("https://www.swiggy.com");
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void signUp() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		loginSignUp_.signUp_.click();
+		loginSignUp_.PhoneNumber.sendKeys("0912345678");
+		loginSignUp_.Name.sendKeys("Awais");
+		loginSignUp_.Email.sendKeys("042waisi@gmail.com");
+		loginSignUp_.Password.sendKeys("awias1234");
+		loginSignUp_.continueBtn.click();
+	}
+
+}
---
+++ Swiggy/src/test/java/SwiggyStepDefinition/loginSignUp.java
@@ -0,0 +1,26 @@
+package SwiggyStepDefinition;
+
+import SwiggyAutomationCases.loginSignUpFunctionality;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class loginSignUp {
+
+	loginSignUpFunctionality loginSignUp_ = new loginSignUpFunctionality();
+	
+	@Given("^User has launch the browser\\.$")
+	public void user_has_launch_the_browser() throws Throwable {
+		loginSignUp_.launchBrowser();		
+	}
+
+	@Then("^Swiggy has been opened\\.$")
+	public void swiggy_has_been_opened() throws Throwable {
+		loginSignUp_.launchSwiggy();
+	}
+
+	@Then("^User enters data in fields required for sign up and clicks continue\\.$")
+	public void user_enters_data_in_fields_required_for_sign_up_and_clicks_continue() throws Throwable {
+		loginSignUp_.signUp();
+	}
+
+}
